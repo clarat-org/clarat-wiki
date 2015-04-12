f193ae80939b2107e6d6f4aa7605a49fa6026082
megadepp <julianjung@engereauswahl.de>
2015-04-12 21:56:00 +0200

    add clarat logo

--- LocalSettings.php
+++ LocalSettings.php
@@ -37,7 +37,7 @@ $wgStylePath = "$wgScriptPath/skins";
 
 ## The relative URL path to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
-$wgLogo = "$wgScriptPath/resources/assets/wiki.png";
+$wgLogo = "$wgScriptPath/images/clarat.gif";
 
 ## UPO means: this is also a user preference option
 
