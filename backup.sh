04a2ef8de69c38fb278c4e9fecf0ea19817abb19
Konstantin K <konstantin.koss@gmail.com>
2015-06-24 18:14:52 +0200

    change name of sft variable because it was empty for some reason

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 export CURRENT_TIME=`date +"%Y-%m-%d-%H-%M-%S"`
 mysqldump --single-transaction -u $DB_USER -p$DB_PWD -h $DB_HOST $DB_TABLE > /tmp/wiki-$CURRENT_TIME.sql
 
-sftp -o "IdentityFile=/app/.ssh/id_rsa" backups@$SFTP_IP:backups <<COMMAND
+sftp -o "IdentityFile=/app/.ssh/id_rsa" backups@$UPLINK:backups <<COMMAND
   put /tmp/wiki-$CURRENT_TIME.sql
   quit
 COMMAND
