421d36ed773ae9783eed535ed955ef9be555049d
Julian Jung <jung@clarat.org>
2015-10-29 17:12:35 +0100

    allow lowercase letters in page titles

--- LocalSettings.php
+++ LocalSettings.php
@@ -210,3 +210,5 @@ $wgDefaultUserOptions['wikieditor-preview'] = 1;
 #require_once "$IP/extensions/CirrusSearch/CirrusSearch.php";
 #require_once "$IP/extensions/Elastica/Elastica.php";
 
+#allow lowercase letter
+$wgCapitalLinks = false;
